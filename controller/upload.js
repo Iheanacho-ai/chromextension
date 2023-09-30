4d84e0704917dd823a95b0f9e320fcceb9c7e32a
Amarachi Iheanacho <amarachi2812@gmail.com>
2023-09-30 22:58:33 +0100

    added the transcribed video

--- controller/upload.js
+++ controller/upload.js
@@ -24,4 +24,29 @@ const uploadVideo = async (req, res) => {
     }
 }
 
-module.exports = { uploadVideo }
+const getTranscribedVideo = async(req, res) => {
+    const publicId = req.params.public_id
+    try {
+        //get the transcribed video from cloudinary using the video's public id
+        const result = await cloudinary.video(publicId, {transformation: [
+            {overlay: {resource_type: "subtitles", public_id: `${publicId}.transcript`}},
+            {flags: "layer_apply"}
+        ]})
+        
+        // return the transcribed video in an html tag 
+        return res.status(200).json({
+            success: true,
+            message: "Video retrieved successfully",
+            data: result
+        });
+        //console.log any error
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: `Error: ${error.message}`
+        })
+    }
+}
+
+module.exports = { uploadVideo, getTranscribedVideo }
--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router();
-const { uploadVideo} = require('../controller/upload')
+const { uploadVideo, getTranscribedVideo} = require('../controller/upload')
 const upload = require('../middleware/multer')
 
 
 router.post('/upload', upload.single('video'), uploadVideo)
+router.get('/:public_id', getTranscribedVideo)
 
 
 
